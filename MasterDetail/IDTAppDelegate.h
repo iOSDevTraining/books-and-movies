c0be1c6f22a3bb8811fbfccd158124782b6d7a8e
Edgar Antunes <edgar10.pt@gmail.com>
2014-01-24 23:04:11 +0000

    Shifted imports to the new @import nomenclature available in iOS7 for faster compilation.

--- MasterDetail/IDTAppDelegate.h
+++ MasterDetail/IDTAppDelegate.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2013 iOS Dev Training. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 @interface IDTAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
